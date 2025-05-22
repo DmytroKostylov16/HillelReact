5f61773bc39f8590eca04053fbfa0928377b7fbb
Dmytro Kostylov <dimakostylov1609@gmail.com>
2025-05-22 11:44:22 +0200

    react setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import ReactComponent from './components/ReactComponent.js'
+function App() {
+  return (
+      <div>
+          <ReactComponent />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/ReactComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ReactComponent extends React.Component {
+
+    render() {
+        return (
+            <>
+                <div>
+                    <span>React Component</span>
+                    <div>
+                        Hello
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+export default ReactComponent;
